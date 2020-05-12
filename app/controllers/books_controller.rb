02dfb36be4640a0273cc0a089bf7dd5aedb8f7b7
Muresan Beniamin <muresan.beniamin@hotmail.com>
2020-05-12 15:56:23 +0300

    Added controller actions, set default route,  added bulma, pry gems, added new book view, index view.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,16 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def new 
+    @book = Book.new
+  end
+
+  def create
+    Book.create(book_params)
+  end
+
+  def book_params
+    params.require(:book).permit!
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_085528) do
+ActiveRecord::Schema.define(version: 2020_05_08_113718) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_05_08_085528) do
     t.integer "year"
     t.integer "rating"
     t.string "status"
+    t.string "image_url"
   end
 
 end
