f1f40e4e04871a8452c534ef88faaab2ab70b194
Muresan Beniamin <muresan.beniamin@hotmail.com>
2020-05-14 14:06:00 +0300

    Fixed create books, changed index page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,9 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    books = Book.all
+    @want_to_read_books = books.where(status: 'Want to Read')
+    @reading_books = books.where(status: 'Reading')
+    @read_books = books.where(status: 'Read')
   end
 
   def new 
@@ -9,6 +12,7 @@ class BooksController < ApplicationController
 
   def create
     Book.create(book_params)
+    redirect_to books_path
   end
 
   def book_params
